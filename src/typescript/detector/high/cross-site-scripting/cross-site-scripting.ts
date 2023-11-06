1e9645fbe0e42ef26b7a217c46466bc55ed70e86
Saurabh Chakraborthy <saurchak@amazon.com>
2023-11-06 09:51:25 +0530

    Modify compliant and non-compliant examples of typescript/server-side-request-forgery@v1.0 and typescript/cross-site-scripting@v1.0

--- src/typescript/detector/high/cross-site-scripting/cross-site-scripting.ts
+++ src/typescript/detector/high/cross-site-scripting/cross-site-scripting.ts
@@ -1,19 +1,21 @@
 // {fact rule=cross-site-scripting@v1.0 defects=1}
 function crossSiteScriptingNoncompliant() {
-  let url = window.location.search.slice(1);
+  let url = window.location.search.slice(1)
+
   // Noncompliant: unsafe jQuery ajax request.
-  $.ajax({ url: url, data: "Hello" });
+  $.ajax({url: url, data: "Hello"})
 }
 // {/fact}
 
 // {fact rule=cross-site-scripting@v1.0 defects=0}
-const ESAPI = require("node-esapi");
+import ESAPI from 'node-esapi'
 
 function crossSiteScriptingCompliant() {
-  let url = window.location.search.slice(1);
-  // Compliant: url is sanitized before ajax call.
-  url = ESAPI.encoder().encodeForURL(url);
-  $.ajax({ url: url, data: "Hello" });
-}
+  let url = window.location.search.slice(1)
 
+  // Compliant: url is sanitized before ajax call.
+  url = ESAPI.encoder().encodeForURL(url)
+
+  $.ajax({url: url, data: "Hello"})
+}
 // {/fact}
