5f68ca17e97daf7e298bb1c5cf94c60013194881
Saurabh Chakraborthy <saurchak@amazon.com>
2023-11-07 20:30:27 +0530

    Modify compliant and non-compliant examples of typescript/open-redirect@v1.0, typescript/stack-trace-exposure@v1.0, typescript/timing-attack@v1.0

--- src/typescript/detector/high/open-redirect/open-redirect.ts
+++ src/typescript/detector/high/open-redirect/open-redirect.ts
@@ -1,41 +1,29 @@
 // {fact rule=open-redirect@v1.0 defects=1}
-var express = require("express");
-var app = express();
+import express, {Request, Response} from 'express'
+var app = express()
 
 function openRedirectNoncompliant() {
-  app.get(
-    "/users/:profileUrl",
-    function (
-      req: { params: { profileUrl: any } },
-      res: { redirect: (arg0: any) => void },
-    ) {
-      var url = req.params.profileUrl;
-      // Noncompliant: user input is used without sanitization.
-      res.redirect(url);
-    },
-  );
+  app.get('/users/:profileUrl',function(req: Request, res: Response) {
+    var url: string = req.params.url
+    // Noncompliant: user input is used without sanitization.
+    res.redirect(url)
+  })
 }
 // {/fact}
 
 // {fact rule=open-redirect@v1.0 defects=0}
-var express = require("express");
-var app = express();
+import express, {Request, Response} from 'express'
+var app = express()
 
 function openRedirectCompliant() {
-  const safeurl = ["www.example.com"];
-  app.post(
-    "/users/:profileUrl",
-    function (
-      req: { params: { profileUrl: any } },
-      res: { redirect: (arg0: string) => any },
-    ) {
-      var url = req.params.profileUrl;
-      if (safeurl.includes(url)) {
-        // Compliant: user input is sanitized before use.
-        return res.redirect(url);
-      }
-      return res.redirect("/");
-    },
-  );
+  const safeurl: string[] = ['www.example.com']
+  app.post('/users/:profileUrl',function(req: Request, res: Response) {
+    var url: string = req.params.url
+    if(safeurl.includes(url)) {
+      // Compliant: user input is sanitized before use.
+      return res.redirect(url)
+    }
+    return res.redirect('/')
+  })
 }
 // {/fact}
