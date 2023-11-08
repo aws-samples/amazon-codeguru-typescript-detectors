aa9eba01b2ad10541a791c0d0bec1191dded719a
akshukul <akshukul@amazon.com>
2023-11-08 13:23:30 +0530

    add syntactical changes to typescript files.

--- src/typescript/detector/high/header-injection/header-injection.ts
+++ src/typescript/detector/high/header-injection/header-injection.ts
@@ -1,34 +1,22 @@
 // {fact rule=header-injection@v1.0 defects=1}
-var express = require("express");
-var app = express();
+import express, { Express, Request, Response } from 'express'
+var app : Express = express()
 function headerInjectionNoncompliant() {
-  app.get(
-    "www.example.com",
-    function (
-      req: { query: { type: any } },
-      res: { setHeader: (arg0: string, arg1: any) => void },
-    ) {
-      // Noncompliant: using untrusted user-input to set response headers.
-      res.setHeader("Content-Type", req.query.type);
-    },
-  );
+  app.get("www.example.com", function (req: Request, res: Response) {
+    // Noncompliant: using untrusted user-input to set response headers.
+    res.setHeader("Content-Type", req.query.type)
+  })
 }
 // {/fact}
 
-// {fact rule=header-injection@v1.0 defects=0}
-var express = require("express");
-var app = express();
-function headerInjectionCompliant() {
-  app.get(
-    "www.example.com",
-    function (
-      req: any,
-      res: { setHeader: (arg0: string, arg1: string) => void },
-    ) {
-      // Compliant: using hardcoded string value to set response headers.
-      res.setHeader("Content-Type", "text/html");
-    },
-  );
-}
 
+// {fact rule=header-injection@v1.0 defects=0}
+import express, { Express, Request, Response } from 'express'
+var app : Express = express()
+function headerInjectionCompliant() {
+  app.get("www.example.com", function (req: Request, res: Response) {
+    // Compliant: using hardcoded string value to set response headers.
+    res.setHeader("Content-Type", "text/html")
+  })
+}
 // {/fact}
--- src/typescript/detector/high/improper-restriction-of-frames/improper-restriction-of-frames.ts
+++ src/typescript/detector/high/improper-restriction-of-frames/improper-restriction-of-frames.ts
@@ -1,33 +1,23 @@
 // {fact rule=improper-restriction-of-frames@v1.0 defects=1}
-var express = require("express");
-var app = express();
+import express, { Express, Request, Response } from 'express'
+var app : Express = express()
 function improperRestrictionOfFramesNoncompliant() {
-  app.use(
-    (
-      req: { query: any },
-      res: { setHeader: (arg0: string, arg1: any) => void },
-    ) => {
-      // Noncompliant: it has broken `X-Frame-Options` header.
-      res.setHeader("X-Frame-Options", req.query);
-    },
-  );
+  app.use((req: Request, res: Response) => {
+    // Noncompliant: it has broken `X-Frame-Options` header.
+    res.setHeader("X-Frame-Options", req.query)
+  })
 }
 // {/fact}
 
-// {fact rule=improper-restriction-of-frames@v1.0 defects=0}
-var express = require("express");
-var app = express();
-function improperRestrictionOfFramesCompliant() {
-  app.use(
-    (
-      req: { query: { opts: any } },
-      res: { setHeader: (arg0: string, arg1: string) => void },
-    ) => {
-      var host = req.query.opts;
-      // Compliant: it has safe `X-Frame-Options` header.
-      res.setHeader("X-Frame-Options", "https://example.com");
-    },
-  );
-}
 
+// {fact rule=improper-restriction-of-frames@v1.0 defects=0}
+import express, { Express, Request, Response } from 'express'
+var app : Express = express()
+function improperRestrictionOfFramesCompliant() {
+  app.use((req: Request, res: Response) => {
+    var host = req.query.opts
+    // Compliant: it has safe `X-Frame-Options` header.
+    res.setHeader("X-Frame-Options", "https://example.com")
+  })
+}
 // {/fact}
