2f2c895e56e10b302dcfa6190d75ef22445e07b9
akshukul <akshukul@amazon.com>
2023-11-08 18:23:30 +0530

    add syntactical changes to typescript files.

--- src/typescript/detector/critical/hardcoded_credentials/hardcoded_credentials.ts
+++ src/typescript/detector/critical/hardcoded_credentials/hardcoded_credentials.ts
@@ -1,55 +1,26 @@
 // {fact rule=hardcoded-credentials@v1.0 defects=1}
-
-function nonCompliant() {
-  var crypto = require("crypto");
-  function hardcodedCredentialsNoncompliant(
-    salt: any,
-    iterations: any,
-    keyLen: any,
-    digest: any,
-  ) {
+var crypto = require('crypto')
+function hardcodedCredentialsNoncompliant(salt: any, iterations: any, keyLen: any, digest: any) {
     // Noncompliant: password is hardcoded.
-    crypto.pbkdf2(
-      "password",
-      salt,
-      iterations,
-      keyLen,
-      digest,
-      (err: any, key: { toString: (arg0: string) => any }) => {
+    crypto.pbkdf2('password', salt, iterations, keyLen, digest, (err: any, key: { toString: (arg0: string) => any }) => {
         if (err) {
-          throw err;
+            throw err
         }
-        return key.toString("base64");
-      },
-    );
-  }
+        return key.toString('base64')
+    })
 }
 // {/fact}
 
-// {fact rule=hardcoded-credentials@v1.0 defects=0}
 
-function compliant() {
-  var crypto = require("crypto");
-  function hardcodedCredentialsCompliant(
-    salt: any,
-    iterations: any,
-    keyLen: any,
-    digest: any,
-  ) {
+// {fact rule=hardcoded-credentials@v1.0 defects=0}
+var crypto = require('crypto')
+function hardcodedCredentialsCompliant(salt: any, iterations: any, keyLen: any, digest: any) {
     // Compliant: password is obtained from environment.
-    crypto.pbkdf2(
-      process.env.password,
-      salt,
-      iterations,
-      keyLen,
-      digest,
-      (err: any, key: { toString: (arg0: string) => any }) => {
+    crypto.pbkdf2(process.env.password, salt, iterations, keyLen, digest, (err: any, key: { toString: (arg0: string) => any }) => {
         if (err) {
-          throw err;
+            throw err
         }
-        return key.toString("base64");
-      },
-    );
-  }
+        return key.toString('base64')
+    })
 }
 // {/fact}
--- src/typescript/detector/critical/session_fixation/session_fixation.ts
+++ src/typescript/detector/critical/session_fixation/session_fixation.ts
@@ -1,35 +1,30 @@
 // {fact rule=session-fixation@v1.0 defects=1}
-var express = require("express");
-var passport = require("passport");
-var app = express();
+import express, { Express, Request, Response } from 'express'
+import passport from 'passport'
+var app :Express = express()
 function sessionFixationNoncompliant() {
-  app.post(
-    "/somepage",
-    passport.authenticate("local", { failureRedirect: "/somepage" }),
-    function (req: any, res: { redirect: (arg0: string) => void }) {
-      // Noncompliant: session.regenerate is absent.
-      res.redirect("/");
-    },
-  );
+  app.post('/somepage',
+      passport.authenticate('local', { failureRedirect: '/somepage' }),
+      function(req: Request, res: Response) {
+        // Noncompliant: session.regenerate is absent.
+        res.redirect('/')
+      })
 }
 // {/fact}
 
+
 // {fact rule=session-fixation@v1.0 defects=0}
-var express = require("express");
-var passport = require("passport");
-var app = express();
+import express, { Express, Request, Response } from 'express'
+import passport from 'passport'
+var app :Express = express()
 function sessionFixationCompliant() {
-  app.post(
-    "/somepage",
-    passport.authenticate("local", { failureRedirect: "/somepage" }),
-    function (
-      req: { session: { regenerate: (arg0: (err: any) => void) => void } },
-      res: { redirect: (arg0: string) => void },
-    ) {
-      // Compliant: session.regenerate is used
-      req.session.regenerate((err: any) => {});
-      res.redirect("/404");
-    },
-  );
+  app.post('/somepage',
+      passport.authenticate('local', { failureRedirect: '/somepage' }),
+      function(req: Request, res: Response) {
+        // Compliant: session.regenerate is used
+        req.session.regenerate((err: any) => {
+        })
+        res.redirect('/404')
+      })
 }
 // {/fact}
